e5c6b59d1d20418eb103e0f55badab45b23b7580
ddhruv8824 <ddhruvsuthar8824@gmail.com>
2025-01-18 12:39:52 +0530

    userprofileNFoodPage

---
+++ front-end/SnackandSteer/src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import UserDetailForm from "./components/CreateProfile/UserDetailForm";
+import FoodPage from "./components/FoodPage/Foodpage";
+
+function App() {
+  return (
+    <>
+      {/* <UserDetailForm /> */}
+      <FoodPage/>
+    </>
+  );
+}
+
+export default App;
