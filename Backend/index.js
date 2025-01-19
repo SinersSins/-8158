638adc523c916a6c5b8a6bb42b9be6f2e2c58752
SinerSins <sinerssins@gmail.com>
2025-01-19 17:17:32 +0530

    Final Commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,53 +1,31 @@
-/** @format */
-
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-const app = express();
-const PORT = process.env.PORT ||3000;
+import path from "path"
 import userProfilerouter from "./routes/userProfile.route.js";
 import userRouter from "./routes/authRoutes.js";
+
+
+const app = express();
+const PORT = process.env.PORT ||3000;
+
+const _dirname = path.resolve()
+
 app.use(cors());
 app.use(express.json());
 app.use("/userProfile", userProfilerouter);
 app.use('/auth', userRouter);
+
+app.use(express.static(path.join(_dirname, "/Frontend/SnackSteer/dist")))
+app.get("*", (req,res)=>{
+  res.sendFile(path.resolve(_dirname,"Frontend","SnackSteer","dist","index.html"))
+})
+
 app.listen(PORT, async () => {
   try {
     await mongoose.connect("mongodb+srv://ddhruv8824:BrSnEqkjXY5IGgUA@cluster0.n4jf8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
-    console.log("server is running");
+    console.log(`Server is running at port ${PORT}`);
   } catch (error){
-    console.error(error,"Error");
+    console.error(error,"Error Connectin to MongoDB");
   }
 });
-
-//////////////////////////////////
-// kranthi's app.js
-
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('./config/db'); // Import database configuration
-// const authRoutes = require('./routes/authRoutes'); // Import authentication routes
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // Routes
-// app.use('/api/auth', authRoutes);
-
-// module.exports = app;
-
-
-
-// const dotenv = require('dotenv');
-// const app = require('./app');
-
-// dotenv.config();
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
